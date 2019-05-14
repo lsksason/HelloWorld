76ae9c924c50c7ad3403c3934e5c0bea8a2e985b
lsk <lskseason@yahoo.com.tw>
2019-05-14 08:41:55 +0800

    Ready

--- Test20190513.py
+++ Test20190513.py
@@ -1,5 +1,5 @@
 
-nums = [2, 7, 11, 15]
+nums = [2, 11, 7, 15]
 target = 9
 
 
@@ -7,9 +7,8 @@ def twoSum(nums, target):
     for i in range(0, len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
-                return i, j
+                return [i, j]
 
 
 print(twoSum(nums, target))
-
 
